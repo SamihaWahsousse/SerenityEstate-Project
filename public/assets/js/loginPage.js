c523770b9420abcee574d3a30ecd6ea590d59c0f
Samiha Wahsousse <wahsousse.samiha@gmail.com>
2023-10-25 11:57:58 +0200

    [WIP-LoginPage]add login page

---
+++ public/assets/js/loginPage.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper-container", {
+  pagination: ".swiper-pagination",
+  paginationClickable: true,
+  parallax: true,
+  speed: 600,
+  autoplay: 3500,
+  loop: true,
+  grabCursor: true
+});
+
+
